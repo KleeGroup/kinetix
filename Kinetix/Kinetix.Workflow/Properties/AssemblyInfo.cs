ad2b48d958075c5d595a4e3d74de030ff086c121
durandx <xavier.durand@mageos.com>
2016-10-07 17:56:39 +0200

    New workflow release

--- Kinetix/Kinetix.Workflow/Properties/AssemblyInfo.cs
+++ Kinetix/Kinetix.Workflow/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.0.28")]
-[assembly: AssemblyFileVersion("0.0.0.28")]
+[assembly: AssemblyVersion("0.0.0.29")]
+[assembly: AssemblyFileVersion("0.0.0.29")]
