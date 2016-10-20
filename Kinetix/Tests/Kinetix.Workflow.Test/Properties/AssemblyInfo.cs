9d22f39149cc3fafad61b965aed8bf724dc10306
durandx <xavier.durand@mageos.com>
2016-10-20 16:48:39 +0200

    moving Workflow and rules tests in 'Test' subfolder

--- Kinetix/Tests/Kinetix.Workflow.Test/Properties/AssemblyInfo.cs
+++ Kinetix/Tests/Kinetix.Workflow.Test/Properties/AssemblyInfo.cs
@@ -2,32 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Fmk.Workflow.Test")]
-[assembly: AssemblyDescription("Tests unitaire workflow")]
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Kinetix.Workflow.Test")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("KLEE Group")]
-[assembly: AssemblyProduct("Fmk.Workflow.Test.Properties")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("a")]
+[assembly: AssemblyProduct("Kinetix.Workflow.Test")]
+[assembly: AssemblyCopyright("Copyright © a 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e796a6c7-1d82-4fe1-9a6a-eecd6d71cee5")]
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("e3f2be57-955f-43ae-9350-c55249b5f2ff")]
 
-// Version information for an assembly consists of the following four values:
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
 //
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les numéros de build et de révision 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
