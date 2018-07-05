a457e41b846f2b900449adbe0340ba47f13bf8a3
durandx <xavier.durand@mageos.com>
2018-07-05 17:11:04 +0200

    [Search] Fix for 15 : Adding AutocompleteResult and AutocompleteResult in Search

---
+++ Kinetix/Kinetix.Search/ComponentModel/AutocompleteItem.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Kinetix.Search.ComponentModel
+{
+    /// <summary>
+    /// Item d'un résultat d'autocomplete.
+    /// </summary>
+    public class AutocompleteItem
+    {
+        /// <summary>
+        /// Clé.
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Libellé.
+        /// </summary>
+        public string Label { get; set; }
+    }
+
+}
+
---
+++ Kinetix/Kinetix.Search/ComponentModel/AutocompleteResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Kinetix.Search.ComponentModel
+{
+    /// <summary>
+    /// Résultat d'une requête d'autocomplete.
+    /// </summary>
+    public class AutocompleteResult
+    {
+        /// <summary>
+        /// Les données.
+        /// </summary>
+        public ICollection<AutocompleteItem> Data { get; set; }
+
+        /// <summary>
+        /// Le nombre total de résultats.
+        /// </summary>
+        public int TotalCount { get; set; }
+    }
+
+}
+
