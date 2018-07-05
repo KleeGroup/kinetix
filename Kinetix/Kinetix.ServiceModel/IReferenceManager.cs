9a2b492299dd1ec5ad41fac7e49dadebd2443657
durandx <xavier.durand@mageos.com>
2018-07-05 11:01:22 +0200

    Fix #17 : Missing methods in IReferenceManager

--- Kinetix/Kinetix.ServiceModel/IReferenceManager.cs
+++ Kinetix/Kinetix.ServiceModel/IReferenceManager.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Kinetix.ServiceModel {
 
@@ -15,12 +17,54 @@ namespace Kinetix.ServiceModel {
         object GetReferenceList(Type referenceType);
 
         /// <summary>
+        /// Retourne la liste de référence du type TReferenceType.
+        /// </summary>
+        /// <param name="referenceName">Nom de la liste de référence à utiliser.</param>
+        /// <typeparam name="TReferenceType">Type de la liste de référence.</typeparam>
+        /// <returns>Liste de référence.</returns>
+        ICollection<TReferenceType> GetReferenceList<TReferenceType>(string referenceName = null);
+
+        /// <summary>
+        /// Retourne les éléments de la liste de référence du type TReference correspondant au prédicat.
+        /// </summary>
+        /// <typeparam name="TReference">Type de la liste de référence.</typeparam>
+        /// <param name="predicate">Prédicat de filtrage.</param>
+        /// <param name="referenceName">Nom de la liste de référence.</param>
+        /// <returns>Ensemble des éléments.</returns>
+        ICollection<TReference> GetReferenceList<TReference>(Func<TReference, bool> predicate, string referenceName = null);
+
+        /// <summary>
         /// Retourne la liste de référence du type referenceType.
         /// </summary>
         /// <param name="referenceType">Type de la liste de référence.</param>
         /// <param name="referenceName">Nom de la liste de référence à utiliser.</param>
         /// <returns>Liste de référence.</returns>
         object GetReferenceList(Type referenceType, string referenceName);
+
+        /// <summary>
+        /// Retourne la liste de référence du type TReferenceType à partir d'un objet de critères.
+        /// </summary>
+        /// <typeparam name="TReferenceType">Type de la liste de référence.</typeparam>
+        /// <param name="criteria">Objet contenant les critères.</param>
+        /// <returns>Les éléments de la liste qui correspondent aux critères.</returns>
+        ICollection<TReferenceType> GetReferenceListByCriteria<TReferenceType>(TReferenceType criteria);
+
+        /// <summary>
+        /// Retourne la liste de référence du type TReferenceType à partir d'un objet de critères.
+        /// </summary>
+        /// <typeparam name="TReferenceType">Type de la liste de référence.</typeparam>
+        /// <typeparam name="TCriteria">Type du critère.</typeparam>
+        /// <param name="criteria">Objet contenant les critères.</param>
+        /// <returns>Les éléments de la liste qui correspondent aux critères.</returns>
+        ICollection<TReferenceType> GetReferenceListByCriteria<TReferenceType, TCriteria>(TCriteria criteria) where TCriteria : TReferenceType;
+
+        /// <summary>
+        /// Retourne un type de référence à partir de sa clef primaire.
+        /// </summary>
+        /// <param name="primaryKey">Clef primaire.</param>
+        /// <typeparam name="TReferenceType">Type de référence.</typeparam>
+        /// <returns>Le type de référence.</returns>
+        TReferenceType GetReferenceObjectByPrimaryKey<TReferenceType>(object primaryKey);
 
         /// <summary>
         /// Retourne un type de référence à partir de sa clef primaire.
@@ -31,12 +75,48 @@ namespace Kinetix.ServiceModel {
         object GetReferenceObjectByPrimaryKey(Type referenceType, object primaryKey);
 
         /// <summary>
+        /// Retourne la valeur d'une liste de référence à partir de son identifiant.
+        /// </summary>
+        /// <typeparam name="TReferenceType">Type de la liste de référence.</typeparam>
+        /// <param name="primaryKey">Clef primaire de l'objet.</param>
+        /// <param name="propertySelector">Lambda expression de sélection de la propriété.</param>
+        /// <returns>Valeur de la propriété sur le bean.</returns>
+        string GetReferenceValueByPrimaryKey<TReferenceType>(object primaryKey, Expression<Func<TReferenceType, object>> propertySelector);
+
+        /// <summary>
+        /// Retourne la liste de référence du type referenceType.
+        /// </summary>
+        /// <param name="primaryKeyArray">Liste des clés primaires.</param>
+        /// <typeparam name="TReferenceType">Type de la liste de référence.</typeparam>
+        /// <returns>Liste de référence.</returns>
+        ICollection<TReferenceType> GetReferenceListByPrimaryKeyList<TReferenceType>(params object[] primaryKeyArray);
+
+        /// <summary>
         /// Retourne la liste de référence du type referenceType.
         /// </summary>
         /// <param name="referenceType">Type de la liste de référence.</param>
         /// <param name="primaryKeyArray">Liste des clés primaires.</param>
         /// <returns>Liste de référence.</returns>
         object GetReferenceListByPrimaryKeyList(Type referenceType, params object[] primaryKeyArray);
+
+        /// <summary>
+        /// Retourne l'élément unique de la liste de référence du type TReference correspondant au prédicat.
+        /// </summary>
+        /// <typeparam name="TReference">Type de la liste de référence.</typeparam>
+        /// <param name="predicate">Prédicat de filtrage.</param>
+        /// <param name="referenceName">Nom de la liste de référence.</param>
+        /// <returns>Ensemble des éléments.</returns>
+        TReference GetReferenceObject<TReference>(Func<TReference, bool> predicate, string referenceName = null);
+
+        /// <summary>
+        /// Retourne la valeur d'une liste de référence à partir
+        /// de son identifiant.
+        /// </summary>
+        /// <typeparam name="TReferenceType">Type de la liste de référence.</typeparam>
+        /// <param name="primaryKey">Identifiant de la liste de référence.</param>
+        /// <param name="defaultPropertyName">Nom de la propriété par défaut à utiliser. Null pour utiliser la valeur définie au niveau de l'objet.</param>
+        /// <returns>Libellé de la liste de référence.</returns>
+        string GetReferenceValueByPrimaryKey<TReferenceType>(object primaryKey, string defaultPropertyName = null);
 
         /// <summary>
         /// Retourne la valeur d'une liste de référence à partir
@@ -46,7 +126,18 @@ namespace Kinetix.ServiceModel {
         /// <param name="primaryKey">Identifiant de la liste de référence.</param>
         /// <param name="defaultPropertyName">Nom de la propriété par défaut à utiliser. Null pour utiliser la valeur définie au niveau de l'objet.</param>
         /// <returns>Libellé de la liste de référence.</returns>
-        string GetReferenceValueByPrimaryKey(Type referenceType, object primaryKey, string defaultPropertyName);
+        string GetReferenceValueByPrimaryKey(Type referenceType, object primaryKey, string defaultPropertyName = null);
+
+        /// <summary>
+        /// Retourne la valeur d'une liste de référence à partir
+        /// de son identifiant et d'une datasource.
+        /// </summary>
+        /// <param name="referenceType">Type de la liste de référence.</param>
+        /// <param name="primaryKey">Identifiant de la liste de référence.</param>
+        /// <param name="dataSource">Datasource.</param>
+        /// <param name="defaultPropertyName">Nom de la propriété par défaut à utiliser. Null pour utiliser la valeur définie au niveau de l'objet.</param>
+        /// <returns>Libellé de la liste de référence.</returns>
+        string GetReferenceValueByPrimaryKey(Type referenceType, object primaryKey, object dataSource, string defaultPropertyName = null);
 
         /// <summary>
         /// Remet à jour le cache pour le type spécifié.
@@ -55,7 +146,7 @@ namespace Kinetix.ServiceModel {
         void FlushCache(Type referenceType);
 
         /// <summary>
-        /// Flisuf all cache.
+        /// Flush all cache.
         /// </summary>
         void FlushAll();
     }
